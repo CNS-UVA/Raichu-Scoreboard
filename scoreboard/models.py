331b3def3fce11b9d63f7f00fcd9a38e4b0cacf2
Alex Shen <shen.alexander@gmail.com>
2023-01-06 23:59:45 -0500

    Updated models

--- scoreboard/models.py
+++ scoreboard/models.py
@@ -1,30 +1,25 @@
 """Models for Scoreboard"""
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
+
 class Team(models.Model):
-    """Team model"""
-    username = models.CharField(max_length=15)
-    password = models.CharField(max_length=100)
-    members = models.CharField(max_length=300)
-    contact = models.EmailField()
-
-class Credential(models.Model):
-    """Credential model.
-
-    Stores credentials provided by users so we can check systems?
-    TODO: Chase what does this do
-    """
-    team_id = models.IntegerField()
+    """Team model. Does not actually store member Users"""
     name = models.CharField(max_length=100)
-    service = models.CharField(max_length=200)
-    ip = models.CharField(max_length=15)
-    port = models.IntegerField()
-    username = models.CharField(max_length=200)
-    password = models.CharField(max_length=200)
+
+    # Make this separate from primary key so we can independently control it
+    team_id = models.IntegerField()
+
+
+class User(AbstractUser):
+    """User model"""
+    team = models.ForeignKey(Team, on_delete=models.CASCADE)
+
 
 class Service(models.Model):
     """Stores information for a given service e.g. FTP, SSH"""
-    service_name = models.CharField(max_length=100)
+    name = models.CharField(max_length=100)
+
 
 class TeamService(models.Model):
     """Stores data about a specific team's service"""
@@ -32,3 +27,16 @@ class TeamService(models.Model):
     service = models.ForeignKey(Service, on_delete=models.CASCADE)
     status = models.CharField(max_length=100)
     message = models.CharField(max_length=200)
+    ip = models.CharField(max_length=15)
+    port = models.IntegerField()
+
+
+class Credential(models.Model):
+    """Credential model.
+
+    Stores credentials provided by users so we can check systems?
+    TODO: Chase what does this do
+    """
+    service = models.ForeignKey(TeamService, on_delete=models.CASCADE)
+    username = models.CharField(max_length=200)
+    password = models.CharField(max_length=200)
