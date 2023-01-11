ed64f2c5371e32b27c8722d4ef0a3c57167f3ea6
Alex Shen <shen.alexander@gmail.com>
2023-01-11 17:32:29 -0500

    Convert mickey into a proper DRF ViewSet

--- scoreboard/urls.py
+++ scoreboard/urls.py
@@ -1,12 +1,20 @@
 """Defines scoreboard URLs"""
-from django.urls import path
+from django.urls import path, include
+from rest_framework import routers
+
 from . import views
+
+router = routers.DefaultRouter()
+router.register(r'credentials', views.CredentialViewSet)
 
 urlpatterns = [
     path('', views.index, name='index'),
     path('credentials', views.credentials, name='credentials'),
     path('services', views.services, name='services'),
     # path('mickey', views.mickey, name='mickey'),
-    path('api/credentials', views.mickey, name='credentials_api'),
-    path('api/scores', views.TheFunction, name='scores_api')
+    # path('api/credentials', views.CredentialViewSet.as_view(), name='credentials_api'),
+    # path('api/scores', views.TheFunction, name='scores_api')
+    path('api/', include(router.urls))
 ]
+
+urlpatterns += router.urls
--- scoreboard/views/api.py
+++ scoreboard/views/api.py
@@ -1,20 +1,15 @@
-# pylint: disable=invalid-name
-# COPE:
 """Views for the API"""
-from django.http import JsonResponse
+# pylint: disable=too-many-ancestors
+# ViewSets have "too many ancestors", but cope
+
+from rest_framework import viewsets
 
 from scoreboard.models import Credential
 from scoreboard.serializers import CredentialSerializer
 
 
-def mickey(request):
-    """View for reading all Credential objects"""
-    # TODO: password protect? ligma cope
-    serializer = CredentialSerializer(Credential.objects.all(), many=True)
-    # TODO: investigate why safe needs to be False
-    return JsonResponse(serializer.data, safe=False)
-
-
-def TheFunction(request):
-    """View for creating new Score objects"""
-    return None
+class CredentialViewSet(viewsets.ModelViewSet):
+    """Handles views for api/credential URL"""
+    queryset = Credential.objects.all()
+    serializer_class = CredentialSerializer
+    # TODO: add auth requirement
